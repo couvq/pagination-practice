660ab4174cfa11c6bc457a043e9daf690f881ae4
couvq <quentincouvelaire@gmail.com>
2022-11-13 16:11:55 -0500

    implemented next button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getUsers } from "./services/getUsers";
+import { useState, useEffect } from "react";
+import { Button, Stack } from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 function App() {
+  const [users, setUsers] = useState(getUsers());
+  const [paginatedUsers, setPaginatedUsers] = useState([]); // we will do pagination with 5 user names at a time
+  const [leftIndex, setLeftIndex] = useState(0);
+  const [rightIndex, setRightIndex] = useState(5);
+  const [isNextDisabled, setIsNextDisabled] = useState(false);
+
+  useEffect(() => {
+    if(leftIndex >= 0 && rightIndex < users.length) {
+      setIsNextDisabled(false);
+      setPaginatedUsers(users.slice(leftIndex, rightIndex));
+    }
+
+    if(rightIndex === users.length || rightIndex > users.length) {
+      setPaginatedUsers(users.slice(leftIndex));
+      setIsNextDisabled(true);
+    }
+  }, [leftIndex, rightIndex]);
+  
+
+  const next = () => {
+    // if rightIndex + 5 <= users.length -> next 5
+    if(rightIndex + 5 <= users.length) {
+      setLeftIndex(leftIndex + 5);
+      setRightIndex(rightIndex + 5);
+    }
+    // if rightIndex + 5 > users.length && rightIndex < users.length -> take the rest of users array
+    else if(rightIndex + 5 > users.length && rightIndex < users.length) {
+      setLeftIndex(leftIndex + 5);
+      setRightIndex(users.length);
+    }
+    // otherwise nextbutton should be disabled
+  };
+
+  const previous = () => {
+    // TODO: implement previous function
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {paginatedUsers.map((user) => (
+        <h1>{user}</h1>
+      ))}
+      <p>
+        Showing users {leftIndex}-{rightIndex} of {users.length}
+      </p>
+      <Stack direction="row" spacing={2}>
+        <Button variant="contained" onClick={previous} disabled={false}>
+          <ChevronLeftIcon />
+          Previous 5 users
+        </Button>
+        <Button variant="contained" onClick={next} disabled={isNextDisabled}>
+          Next 5 users
+          <ChevronRightIcon />
+        </Button>
+      </Stack>
+    </>
   );
 }
 
---
+++ src/services/getUsers.js
@@ -0,0 +1,15 @@
+export const getUsers = () => {
+    return [
+        "0 Josh",
+        "1 Maddy",
+        "2 Allen",
+        "3 Jake",
+        "4 Dale",
+        "5 Jeff",
+        "6 Quentin",
+        "7 Tristan",
+        "8 Didier",
+        "9 Mohammed",
+        "10 Jason"
+    ]
+}
